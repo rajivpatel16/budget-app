059abc3d6a238379d925b6305e4e3e91e348bb75
rajivpatel16 <rajivpatel16@gmail.com>
2021-01-14 17:30:34 +0530

    updated port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = process.env.port || 3000;
+//const port = process.env.port || 3000;
 
 app.use(express.static(path.join(__dirname, 'build')));
 
@@ -9,6 +9,6 @@ app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(port, () => {
+app.listen(3000, () => {
     console.log("server is running up")
 });
